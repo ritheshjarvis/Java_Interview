e5686d57cd6b27e2003b3e5b0f509ee9de4a5dea
Rithesh Bhaskara <rithesh.bhaskara@honeywell.com>
2025-02-18 21:00:41 +0530

    occurrence of string

---
+++ src/test/java/programs/OccurrenceOfCharacter.java
@@ -0,0 +1,24 @@
+// Find the Occurrence of character in a given string
+
+package programs;
+
+import java.util.HashMap;
+
+public class OccurrenceOfCharacter {
+    public static void main(String[] args){
+        String input = "aabbcdefg";
+        char[] input_arr = input.toCharArray();
+
+        HashMap<Character, Integer> output = new HashMap<>();
+
+        for(Character ch : input_arr){
+            if (output.containsKey(ch)) {
+                output.put(ch, output.get(ch) + 1);
+            } else {
+                output.put(ch, 1);
+            }
+        }
+
+        System.out.println(output);
+    }
+}
